67c94ae7316424189eb024a4fbf3a229b1ee72c4
Nine-Tailed Fox <yugenshiro@proton.me>
2026-02-10 20:07:47 +0000

    Implemented symlog model training

--- symlog/symlog_train.py
+++ symlog/symlog_train.py
@@ -1,37 +1,51 @@
 import torch
+from torch.serialization import save
 from symlog_net import SymLog
+import wandb
+from tqdm import tqdm
 
-BATCH_SIZE = 4096
-GENERATION_SIZE = 1_000_000
-LOW_BOUND: int = -100_000
-HIGH_BOUND: int = 100_000
-LR: float = 0.001
-
-TEST_BATCH_SIZE = 1_000_000
-TEST_INTERVAL = 1_000
-
-symlog_net = SymLog()
-optimizer = torch.optim.AdamW(symlog_net.parameters(), lr=LR)
-
-def test(symlog_net: SymLog) -> None:
-    input = torch.rand((TEST_BATCH_SIZE, 1)) + torch.randint(LOW_BOUND, HIGH_BOUND, size=[TEST_BATCH_SIZE, 1])
+def test(symlog_net: SymLog, run: wandb.Run) -> torch.Tensor:
+    input = torch.rand((run.config['test_batch_size'], 1)) + torch.randint(run.config['low_bound'], run.config['high_bound'], size=[run.config['test_batch_size'], 1])
     true_values = torch.sign(input) * torch.log(torch.abs(input) + 1)
     predicted_values = symlog_net(input)
 
     loss = torch.mean(((true_values - predicted_values) ** 2)) / 2
-    print(f"Test Loss: {loss}")
+    return loss
+def train(wandb_run: wandb.Run, symlog_net: SymLog, optimizer: torch.optim.Optimizer):
+    for generation in tqdm(range(wandb_run.config['generation_size']), desc="Training"):
+        input = torch.rand((wandb_run.config['batch_size'], 1)) + torch.randint(wandb_run.config['low_bound'], wandb_run.config['high_bound'], size=[wandb_run.config['batch_size'], 1])
+        true_values = torch.sign(input) * torch.log(torch.abs(input) + 1)
+        predicted_values = symlog_net(input)
 
-for generation in range(GENERATION_SIZE):
-    input = torch.rand((BATCH_SIZE, 1)) + torch.randint(LOW_BOUND, HIGH_BOUND, size=[BATCH_SIZE, 1])
-    true_values = torch.sign(input) * torch.log(torch.abs(input) + 1)
-    predicted_values = symlog_net(input)
+        loss = torch.mean(((true_values - predicted_values) ** 2)) / 2
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        run.log({"loss": loss, "generation": generation})
+        if generation % config['test_generation_interval'] == 0:
+            test_loss = test(symlog_net, wandb_run)
+            run.log({"test loss": test_loss, "generation": generation})
 
-    loss = torch.mean(((true_values - predicted_values) ** 2)) / 2
-    loss.backward()
-    optimizer.step()
-    optimizer.zero_grad()
-    if generation % TEST_INTERVAL == 0:
-        test(symlog_net)
-        print(f"Generation: {generation}")
-    else:
-        print(f"Generation: {generation}", end='\r')
+    save_model(symlog_net, wandb_run.config['save_path'])
+
+def save_model(model: torch.nn.Module, path: str):
+    torch.save(model.state_dict(), path)
+
+if __name__ == '__main__':
+    project_name = "Dreamer-V3"
+    path = './model/symlog.pt'
+    config = {
+        'lr': 0.001,
+        'batch_size': 4096,
+        'generation_size': 1_000_000,
+        'low_bound': -100_000,
+        'high_bound': 100_000,
+        'test_batch_size': 1_000_000,
+        'test_generation_interval': 1_000,
+        'save_path': path,
+    }
+
+    with wandb.init(project=project_name, config=config, mode="online") as run:
+        symlog_net = SymLog()
+        optimizer = torch.optim.AdamW(symlog_net.parameters(), lr=run.config['lr'])
+        train(run, symlog_net, optimizer)
