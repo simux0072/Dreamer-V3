04df30f021662ec01fa53c39671a29c6557cd03a
Nine-Tailed Fox <yugenshiro@proton.me>
2026-02-13 04:39:40 +0000

    Trained the symlog network

--- symlog/symlog_net.py
+++ symlog/symlog_net.py
@@ -1,5 +1,6 @@
 import torch
 
+
 class SymLog(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
@@ -11,10 +12,14 @@ class SymLog(torch.nn.Module):
             torch.nn.SELU(),
             torch.nn.Linear(16, 8),
             torch.nn.SELU(),
-            torch.nn.Linear(8, 1)
+            torch.nn.Linear(8, 1),
         )
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
+        if input.size(-1) != 1:
+            initial_shape = input.shape
+            transformed_input = input.flatten(start_dim=1).unsqueeze(dim=-1)
+            prediction = self.symlog(transformed_input).reshape(initial_shape)
+            return prediction
         prediction = self.symlog(input)
         return prediction
-
