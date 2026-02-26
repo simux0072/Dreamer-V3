aeb617100be499f3b731899fc8603c1905bbcd67
Nine-Tailed Fox <yugenshiro@proton.me>
2026-02-26 09:12:33 +0000

    Added GRU model functions and encoder and hidden update functions

--- network.py
+++ network.py
@@ -1,51 +1,124 @@
 import torch
 
+
 class Dreamer_V3(torch.nn.Module):
-    def __init__(self, symlog_model: torch.nn.Module) -> None:
+    def __init__(
+        self, symlog_model: torch.nn.Module, device: torch.device, batch_size: int = 16
+    ) -> None:
         super().__init__()
-        self.symlog = symlog_model
-        #Initial image input size: 20x20
-        # Use CNNs for parsing the image into latent space
+        self.device = device
+        self.symlog = symlog_model.to(device=self.device)
+        self.batch_size = batch_size
+
+        self.current_hidden = torch.zeros((self.batch_size, 1024))
+        self.current_action = torch.zeros((self.batch_size, 1))
+        self.current_latent = torch.zeros((self.batch_size, 1024))
+        self.hidden_cell = torch.nn.GRUCell(
+            input_size=1025, hidden_size=1024, device=self.device
+        )
+
         self.encoder = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=1, out_channels=32, kernel_size=4, stride=2, padding=1),
-            torch.nn.LayerNorm([32, 10, 10]),
+            torch.nn.Conv2d(
+                in_channels=1,
+                out_channels=32,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                device=self.device,
+            ),
+            torch.nn.LayerNorm([32, 10, 10], device=self.device),
             torch.nn.SiLU(),
-            torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, stride=2, padding=1),
-            torch.nn.LayerNorm([64, 6, 6]),
+            torch.nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=2,
+                stride=2,
+                padding=1,
+                device=self.device,
+            ),
+            torch.nn.LayerNorm([64, 6, 6], device=self.device),
             torch.nn.SiLU(),
-            torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=2, stride=2, padding=1),
-            torch.nn.LayerNorm([128, 4, 4]),
+            torch.nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=2,
+                stride=2,
+                padding=1,
+                device=self.device,
+            ),
+            torch.nn.LayerNorm([128, 4, 4], device=self.device),
             torch.nn.SiLU(),
-            torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=1),
-            torch.nn.LayerNorm([256, 4, 4]),
+            torch.nn.Conv2d(
+                in_channels=128, out_channels=256, kernel_size=1, device=self.device
+            ),
+            torch.nn.LayerNorm([256, 4, 4], device=self.device),
             torch.nn.SiLU(),
         )
 
         self.decoder = torch.nn.Sequential(
-            torch.nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=1),
-            torch.nn.LayerNorm([128, 4, 4]),
+            torch.nn.ConvTranspose2d(
+                in_channels=256, out_channels=128, kernel_size=1, device=self.device
+            ),
+            torch.nn.LayerNorm([128, 4, 4], device=self.device),
             torch.nn.SiLU(),
-            torch.nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2, padding=1),
-            torch.nn.LayerNorm([64, 6, 6]),
+            torch.nn.ConvTranspose2d(
+                in_channels=128,
+                out_channels=64,
+                kernel_size=2,
+                stride=2,
+                padding=1,
+                device=self.device,
+            ),
+            torch.nn.LayerNorm([64, 6, 6], device=self.device),
             torch.nn.SiLU(),
-            torch.nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=2, stride=2, padding=1),
-            torch.nn.LayerNorm([32, 10, 10]),
+            torch.nn.ConvTranspose2d(
+                in_channels=64,
+                out_channels=32,
+                kernel_size=2,
+                stride=2,
+                padding=1,
+                device=self.device,
+            ),
+            torch.nn.LayerNorm([32, 10, 10], device=self.device),
             torch.nn.SiLU(),
-            torch.nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=4, stride=2, padding=1),
+            torch.nn.ConvTranspose2d(
+                in_channels=32,
+                out_channels=1,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                device=self.device,
+            ),
+        )
+
+        self.latent = torch.nn.Sequential(
+            torch.nn.Linear(in_features=5120, out_features=8192, device=self.device),
+            torch.nn.SELU(),
+            torch.nn.Linear(in_features=8192, out_features=4096, device=self.device),
+            torch.nn.SELU(),
+            torch.nn.Linear(in_features=4096, out_features=2048, device=self.device),
+            torch.nn.SELU(),
+            torch.nn.Linear(in_features=2048, out_features=1024, device=self.device),
         )
 
     def forward(self):
         pass
+
     def symlog_transform(self, input: torch.Tensor) -> torch.Tensor:
         transformed_input = self.symlog(input)
         return transformed_input
 
-    def test_encoder(self, input: torch.Tensor) -> torch.Tensor:
-        transformed_input = self.symlog_transform(input)
-        output = self.encoder(transformed_input)
-        return output
-    
-    def test_decoder(self, input: torch.Tensor) -> torch.Tensor:
-        symlog_input = self.symlog(input)
-        output = self.decoder(symlog_input)
-        return output
+    def encoder(self, input_image: torch.Tensor):
+        encoder_out: torch.Tensor = self.encoder(input_image)
+        return encoder_out
+
+    def hidden_pass(self):
+        concat_hidden = torch.concat((self.current_latent, self.current_action))
+        self.current_hidden = self.hidden_cell(concat_hidden, self.current_hidden)
+
+    def latent_pass(self, encoder_out: torch.Tensor):
+        input = torch.concat((encoder_out, self.current_hidden))
+        self.current_latent = self.latent(input)
+        self.current_dist = torch.reshape(
+            self.current_latent, shape=(self.batch_size, 32, 32)
+        )
